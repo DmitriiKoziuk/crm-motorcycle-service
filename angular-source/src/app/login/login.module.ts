a6f22e7b4b517b155ba571248afed7668e471ea0
Kynica <dmitry.kozyuk@gmail.com>
2017-12-05 21:20:46 +0200

    Create LoginModule/Component.

--- angular-source/src/app/login/login.module.ts
+++ angular-source/src/app/login/login.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login.component';
+import { MaterialModule } from '../shared/modules/material.module';
 
 const loginRoutes: Routes = [
   {path: '', component: LoginComponent}
@@ -13,8 +15,10 @@ const loginRoutes: Routes = [
     LoginComponent,
   ],
   imports: [
+    MaterialModule,
     CommonModule,
     RouterModule.forChild(loginRoutes),
+    ReactiveFormsModule,
   ],
 })
 export class LoginModule { }
