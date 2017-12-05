91756dc86c2a4db1620c21539effe6537400ac59
Kynica <dmitry.kozyuk@gmail.com>
2017-12-05 12:49:56 +0200

    Add property access_token and method generateAccessToken().

--- common/models/User.php
+++ common/models/User.php
@@ -20,6 +20,7 @@ use yii\web\IdentityInterface;
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
+ * @property string $access_token
  */
 class User extends ActiveRecord implements IdentityInterface
 {
@@ -169,6 +170,11 @@ class User extends ActiveRecord implements IdentityInterface
     public function generateAuthKey()
     {
         $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    public function generateAccessToken()
+    {
+        $this->access_token = Yii::$app->security->generateRandomString();
     }
 
     /**
