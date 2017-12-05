97f6c09bcc1c3b492846727485262946cffb394d
Kynica <dmitry.kozyuk@gmail.com>
2017-12-05 21:21:07 +0200

    Add modules and services to app.module.ts

--- angular-source/src/app/app.module.ts
+++ angular-source/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
+import { MaterialModule } from './shared/modules/material.module';
+
+import { DebugService } from './shared/services/debug.service';
+import { ApiService } from './shared/services/api.service';
+import { UserService } from './shared/services/user.service';
 
 
 @NgModule({
@@ -12,10 +20,19 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule,
     AppRoutingModule,
+    HttpClientModule,
+    MaterialModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    DebugService,
+    ApiService,
+    UserService,
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
