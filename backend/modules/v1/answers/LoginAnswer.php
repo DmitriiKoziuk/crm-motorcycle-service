99dcdfa5faaa91fa5be49e7658ceabf36400c656
Kynica <dmitry.kozyuk@gmail.com>
2017-12-07 16:21:57 +0200

    Create login answer class.

---
+++ backend/modules/v1/answers/LoginAnswer.php
@@ -0,0 +1,18 @@
+<?php
+namespace backend\modules\v1\answers;
+
+use backend\modules\v1\models\User;
+
+class LoginAnswer
+{
+    public static function get(User $user)
+    {
+        return [
+            'message'      => "Hello {$user->username}",
+            'id'           => $user->id,
+            'username'     => $user->username,
+            'access-token' => $user->access_token,
+            'profile'      => $user->profile,
+        ];
+    }
+}
