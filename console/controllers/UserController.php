eed9c31b66574a7ea331996b29ed7ecadcdccc25
Kynica <dmitry.kozyuk@gmail.com>
2017-12-07 13:58:38 +0200

    Create console script user for create and delete user.

---
+++ console/controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+use yii\helpers\Console;
+use console\models\User;
+
+class UserController extends Controller
+{
+    public function actionIndex()
+    {
+        echo 'Hello' . "\n";
+    }
+
+    public function actionCreate($username, $email = 'someFakeUserEmail')
+    {
+        try {
+            $password = Yii::$app->security->generateRandomString(10);
+
+            User::create($username, $email, $password);
+
+            $this->stdout("User created, name - '{$username}', password - '{$password}'.\n", Console::FG_GREEN);
+
+            return 0;
+        } catch (\Exception $e) {
+            $this->stdout($e->getMessage(), Console::FG_RED);
+            return 1;
+        }
+    }
+
+    public function actionDelete($username)
+    {
+        try {
+            $user = User::find()
+                ->where(['username' => $username])
+                ->one();
+
+            if (empty($user)) {
+                throw new \Exception("User with name '{$username}' do not exist.\n");
+            }
+
+            $user->delete();
+
+            $this->stdout("User '{$username}' is deleted.\n", Console::FG_GREEN);
+            return 0;
+        } catch (\Exception $e) {
+            $this->stdout($e->getMessage(), Console::FG_RED);
+            return 1;
+        }
+    }
+}
