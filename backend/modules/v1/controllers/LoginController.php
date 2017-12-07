a32dfd0b6acee4d2a8662957237587f2a6c05e7c
Kynica <dmitry.kozyuk@gmail.com>
2017-12-07 20:09:00 +0200

    Add actionCheck() for checking is access_token steel actual.

--- backend/modules/v1/controllers/LoginController.php
+++ backend/modules/v1/controllers/LoginController.php
@@ -3,6 +3,7 @@ namespace backend\modules\v1\controllers;
 
 use Yii;
 use yii\db\Exception;
+use yii\db\Expression;
 use yii\rest\Controller;
 use yii\filters\Cors;
 use backend\forms\LoginForm;
@@ -44,9 +45,10 @@ class LoginController extends Controller
             /** @var User $user */
             $user = User::find()
                 ->with(['profile'])
-                ->where([
-                    'username' => $loginForm->username
-                ])->one();
+                ->where(
+                    ['username' => new Expression(':username')],
+                    [':username' => $loginForm->username]
+                )->one();
 
             if (empty($user))
                 throw new Exception("User '{$loginForm->username}' not found");
@@ -55,6 +57,37 @@ class LoginController extends Controller
                 throw new \Exception("Incorrect password");
 
             $answer = LoginAnswer::get($user);
+        } catch (\Exception $e) {
+            $answer['error'] = $e->getMessage();
+        }
+
+        return $answer;
+    }
+
+    public function actionCheck()
+    {
+        $answer = [];
+
+        try {
+            $accessToken = Yii::$app->getRequest()->getBodyParam('access_token');
+            $method      = Yii::$app->getRequest()->getMethod();
+
+            if ($method === 'POST') {
+                $user = User::find()
+                    ->where(
+                        ['access_token' => new Expression(':token')],
+                        [':token' => $accessToken]
+                    )
+                    ->one();
+
+                if (empty($user)) {
+                    throw new Exception("Access token not found");
+                }
+            }
+
+            $answer = [
+                'message' => 'ok'
+            ];
         } catch (\Exception $e) {
             $answer['error'] = $e->getMessage();
         }
