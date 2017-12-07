a6c2c832515f858d4c80ab408d6e5456e487774f
Kynica <dmitry.kozyuk@gmail.com>
2017-12-07 13:57:31 +0200

    Move create() method to common\User.

--- backend/models/User.php
+++ backend/models/User.php
@@ -1,26 +1,8 @@
 <?php
 namespace backend\models;
 
-use yii\db\Exception;
 use common\models\User as U;
 
-class User extends U {
-    public static function create($username, $email, $password) {
-        $user           = new static();
-        $user->username = $username;
-        $user->email    = $email;
-        $user->setPassword($password);
-        $user->generateAuthKey();
-        $user->generateAccessToken();
-
-        if (! $user->validate()) {
-            throw new \Exception('Not valid data');
-        }
-
-        if (! $user->save()) {
-            throw new Exception("Can't save user");
-        }
-
-        return true;
-    }
+class User extends U
+{
 }
