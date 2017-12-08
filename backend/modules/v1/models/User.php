8dab5136f4bbc6f49d8b615b2df2d02e98215c6f
Kynica <dmitry.kozyuk@gmail.com>
2017-12-08 16:05:08 +0200

    Add method for checking login status.

--- backend/modules/v1/models/User.php
+++ backend/modules/v1/models/User.php
@@ -5,6 +5,7 @@ use yii\db\Exception;
 use yii\db\Expression;
 use common\models\User as U;
 use backend\modules\v1\requests\LoginRequest;
+use backend\modules\v1\requests\LoginCheckRequest;
 
 class User extends U
 {
@@ -23,6 +24,27 @@ class User extends U
 
         if (! $user->validatePassword($request->password))
             throw new \Exception("Incorrect username or password");
+
+        return $user;
+    }
+
+    public static function checkLogin(LoginCheckRequest $request)
+    {
+        $user = User::find()
+            ->where(
+                [
+                    'username'     => new Expression(':username'),
+                    'access_token' => new Expression(':access_token'),
+                ],
+                [
+                    ':username'     => $request->username,
+                    ':access_token' => $request->access_token
+                ]
+            )->one();
+
+        if (empty($user)) {
+            throw new Exception("Access token not found");
+        }
 
         return $user;
     }
