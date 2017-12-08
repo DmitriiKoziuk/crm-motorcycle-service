fba5f7d26d02353603ef4f535216184a3f6e7551
Kynica <dmitry.kozyuk@gmail.com>
2017-12-08 14:09:20 +0200

    User: 1. Create static method User::login(LoginRequest $request).

--- backend/modules/v1/models/User.php
+++ backend/modules/v1/models/User.php
@@ -1,8 +1,29 @@
 <?php
 namespace backend\modules\v1\models;
 
+use yii\db\Exception;
+use yii\db\Expression;
 use common\models\User as U;
+use backend\modules\v1\requests\LoginRequest;
 
 class User extends U
 {
+    public static function login(LoginRequest $request)
+    {
+        /** @var User $user */
+        $user = User::find()
+            ->with(['profile'])
+            ->where(
+                ['username' => new Expression(':username')],
+                [':username' => $request->username]
+            )->one();
+
+        if (empty($user))
+            throw new Exception("User '{$request->username}' not found");
+
+        if (! $user->validatePassword($request->password))
+            throw new \Exception("Incorrect username or password");
+
+        return $user;
+    }
 }
