fc884d661c56a64efcbf4d4cf6eb10c1af13da41
Kynica <dmitry.kozyuk@gmail.com>
2017-12-09 21:46:27 +0200

    Move links from template to class.

--- angular-source/src/app/components/sidebar/sidebar.component.ts
+++ angular-source/src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,16 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  links = [
+    {name: 'dashboard', url: ['/dashboard'], permissionName: 'seeDashboardPage', iconName: 'desktop'},
+    {name: 'users', url: ['/user'], permissionName: 'seeUserControlPage', iconName: ''},
+  ];
+
+  constructor(public userService: UserService) { }
+}
