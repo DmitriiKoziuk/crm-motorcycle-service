4ef3710132f658f6db1a9cfb45bd280d2389a0d3
Kynica <dmitry.kozyuk@gmail.com>
2017-12-09 12:21:15 +0200

    Add authenticator HttpBearerAuth.

--- backend/modules/v1/controllers/UserController.php
+++ backend/modules/v1/controllers/UserController.php
@@ -3,18 +3,23 @@ namespace backend\modules\v1\controllers;
 
 use yii\rest\Controller;
 use yii\filters\Cors;
+use yii\filters\auth\HttpBearerAuth;
 use backend\modules\v1\models\User;
 
 class UserController extends Controller
 {
     public function behaviors()
     {
-        $behaviours = parent::behaviors();
-        $behaviours['corsFilter'] = [
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::className(),
+            'except' => ['options'],
+        ];
+        $behaviors['corsFilter'] = [
             'class' => Cors::className(),
         ];
 
-        return $behaviours;
+        return $behaviors;
     }
 
     public function actions()
