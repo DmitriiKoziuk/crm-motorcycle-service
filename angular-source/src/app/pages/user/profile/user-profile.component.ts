fedfd09b989e524f04067586214bae36bb3d1e06
Kynica <dmitry.kozyuk@gmail.com>
2017-12-10 20:50:30 +0200

    Add roles and loading them from api.

--- angular-source/src/app/pages/user/profile/user-profile.component.ts
+++ angular-source/src/app/pages/user/profile/user-profile.component.ts
@@ -5,6 +5,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { UserService } from '../../../shared/services/user.service';
 import { User } from '../../../shared/models/user';
 import { EmptyUser } from '../../../shared/models/empty-user';
+import { RoleService } from '../../../shared/services/role.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -15,18 +16,25 @@ export class UserProfileComponent implements OnInit {
   title = 'Edit user';
   user: User;
   form: FormGroup;
+  roles;
 
   constructor(
     private userService: UserService,
-    private route: ActivatedRoute,
+    private route:       ActivatedRoute,
+    private roleService: RoleService,
   ) {}
 
   ngOnInit() {
     this.user = new EmptyUser();
 
+    this.roleService.getAll().subscribe((data) => {
+      this.roles = data;
+    });
+
     this.form = new FormGroup({
       username: new FormControl(''),
       password: new FormControl(''),
+      role:     new FormControl(''),
       profile:  new FormGroup({
         first_name:       new FormControl(''),
         family_name:      new FormControl(''),
