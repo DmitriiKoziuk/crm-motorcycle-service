db05b9696825e8b6a882819364f1701b7c0c4e61
Kynica <dmitry.kozyuk@gmail.com>
2017-12-10 19:11:57 +0200

    Create EmptyUser model.

---
+++ angular-source/src/app/shared/models/empty-user.ts
@@ -0,0 +1,21 @@
+import { User } from './user';
+
+export class EmptyUser extends User {
+  id           = '';
+  access_token = '';
+  username     = '';
+
+  profile = {
+    first_name:       '',
+    family_name:      '',
+    middle_name:      '',
+    photo:            '',
+    telephone_number: '',
+  };
+
+  role: {
+    name: '',
+  };
+
+  permissions = {};
+}
