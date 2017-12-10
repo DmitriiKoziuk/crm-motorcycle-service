116b1c3e06eb64205a0aff44678ad8917665ec1c
Kynica <dmitry.kozyuk@gmail.com>
2017-12-10 15:08:56 +0200

    Some code style fix.

--- angular-source/src/app/pages/user/profile/user-profile.module.ts
+++ angular-source/src/app/pages/user/profile/user-profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { UserProfileComponent } from './user-profile.component';
 import { RouterModule, Routes } from '@angular/router';
+
+import { UserProfileComponent } from './user-profile.component';
 
 const ROUTES: Routes = [
   {path: '', component: UserProfileComponent},
