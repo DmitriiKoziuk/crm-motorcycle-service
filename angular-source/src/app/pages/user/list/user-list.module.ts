57aaba9a3e940935c69014db23cf44aa306ddb53
Kynica <dmitry.kozyuk@gmail.com>
2017-12-10 14:41:10 +0200

    Some code style fix.

--- angular-source/src/app/pages/user/list/user-list.module.ts
+++ angular-source/src/app/pages/user/list/user-list.module.ts
@@ -1,10 +1,10 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule } from '@angular/router';
 
-import {UserListComponent} from './user-list.component';
-import {MaterialModule} from '../../../shared/modules/material.module';
-import {TelephonePipe} from '../../../shared/pipes/telephone.pipe';
+import { UserListComponent } from './user-list.component';
+import { MaterialModule} from '../../../shared/modules/material.module';
+import { TelephonePipe } from '../../../shared/pipes/telephone.pipe';
 
 @NgModule({
   declarations: [
