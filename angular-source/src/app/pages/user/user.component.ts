a3734bf4a9015d9f53ef070c7d099f4f79fd9d01
Kynica <dmitry.kozyuk@gmail.com>
2017-12-10 14:41:44 +0200

    Move user list to user-list module.

--- angular-source/src/app/pages/user/user.component.ts
+++ angular-source/src/app/pages/user/user.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
-
-import { UserService } from '../../shared/services/user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  title            = 'Users control';
-  displayedColumns = ['id', 'photo', 'role', 'login', 'name', 'tel'];
-  dataSource;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.dataSource = new MatTableDataSource();
-    this.userService.getAll().subscribe(data => {
-      this.dataSource.data = data;
-    });
-  }
-}
+export class UserComponent {}
