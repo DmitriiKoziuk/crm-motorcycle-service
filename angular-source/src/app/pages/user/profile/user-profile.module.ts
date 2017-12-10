257f3b0208fad427fab913295e55d6e699cbe8f9
Kynica <dmitry.kozyuk@gmail.com>
2017-12-10 20:49:58 +0200

    Add CommonModule.

--- angular-source/src/app/pages/user/profile/user-profile.module.ts
+++ angular-source/src/app/pages/user/profile/user-profile.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -14,6 +15,7 @@ const ROUTES: Routes = [
     UserProfileComponent
   ],
   imports: [
+    CommonModule,
     RouterModule.forChild(ROUTES),
     MaterialModule,
     FormsModule,
