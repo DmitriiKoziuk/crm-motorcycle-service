f9903750d4493c38347f853f12885ecdb0f72bc9
Kynica <dmitry.kozyuk@gmail.com>
2018-02-21 10:12:10 +0200

    Create user search form.

---
+++ angular-source/src/app/pages/user/list/search-form/user-list-search-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user-list-search-from',
+  templateUrl: './user-list-search-form.component.html',
+  styleUrls: ['./user-list-search-form.component.css']
+})
+export class UserListSearchFormComponent implements OnInit {
+  form = new FormGroup({
+    username: new FormControl(''),
+  });
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router:         Router
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.form.patchValue(params);
+    });
+  }
+
+  search() {
+    this.router.navigate(['/user'], {queryParams: this.form.getRawValue()});
+  }
+
+  reset() {
+    this.form.reset();
+    this.router.navigate(['/user']);
+  }
+}
