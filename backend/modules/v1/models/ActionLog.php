65303b5f8bf4f7fb2b3751bf3f74bd7e3537b2ec
Kynica <dmitry.kozyuk@gmail.com>
2018-03-01 18:23:23 +0200

    Create: 1. action_log table prototype in mysql workbanch; 2. Migration for table action_log; 3. Model ActionLog for action_log table; 4. Method write() for writing log to database.

---
+++ backend/modules/v1/models/ActionLog.php
@@ -0,0 +1,8 @@
+<?php
+namespace backend\modules\v1\models;
+
+use common\models\ActionLog as AL;
+
+class ActionLog extends AL
+{
+}
---
+++ console/migrations/m180301_131955_create_action_log_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `action_log`.
+ */
+class m180301_131955_create_action_log_table extends Migration
+{
+    protected $tableName = '{{%action_log}}';
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable($this->tableName, [
+            'id'          => $this->primaryKey(),
+            'user_id'     => $this->integer()->notNull(),
+            'entity_name' => $this->string(45)->notNull(),
+            'entity_id'   => $this->string(45)->notNull()->defaultValue(''),
+            'action_type' => $this->integer()->notNull(),
+            'old_data'    => $this->text()->notNull()->defaultValue(''),
+            'new_data'    => $this->text()->notNull()->defaultValue(''),
+            'apply_time'  => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex(
+            'idx-action_log-user_id',
+            $this->tableName,
+            'user_id'
+        );
+
+        $this->addForeignKey(
+            'fk-action_log-user_id',
+            $this->tableName,
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $this->dropForeignKey('fk-action_log-user_id', $this->tableName);
+        $this->dropTable($this->tableName);
+    }
+}
