8fbe8bf8c2d4eb101935157c6124e56265bf2e5a
Kynica <dmitry.kozyuk@gmail.com>
2018-03-04 12:19:00 +0200

    Use VehicleRecord.

--- angular-source/src/app/pages/vehicle/list/vehicle-list.component.ts
+++ angular-source/src/app/pages/vehicle/list/vehicle-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PageTitleService } from '../../../shared/services/page-title.service';
+import { VehicleRecord } from '../../../shared/records/vehicle.record';
 
 @Component({
   selector: 'app-pages-vehicle-list',
   templateUrl: './vehicle-list.component.html',
+  providers: [VehicleRecord]
 })
 export class VehicleListComponent implements OnInit {
   constructor(
