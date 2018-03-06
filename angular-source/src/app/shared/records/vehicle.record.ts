a39502d541a46c0cb9ec597443e88a5de780d016
Kynica <dmitry.kozyuk@gmail.com>
2018-03-06 23:19:25 +0200

    Add attributes.

--- angular-source/src/app/shared/records/vehicle.record.ts
+++ angular-source/src/app/shared/records/vehicle.record.ts
@@ -1,6 +1,30 @@
 import { ActiveRecord } from './active.record';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export class VehicleRecord extends ActiveRecord {
+  maxLength = 150;
+
+  attributes = new FormGroup({
+    id:   new FormControl(''),
+    type: new FormGroup({
+      id:   new FormControl(''),
+      name: new FormControl('', [
+        Validators.required,
+      ]),
+    }),
+    brand: new FormGroup({
+      id:   new FormControl(''),
+      name: new FormControl('', [
+        Validators.required,
+      ]),
+    }),
+    model_name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(this.maxLength),
+    ]),
+  });
+
   public getUrl() {
     return '/vehicle';
   }
